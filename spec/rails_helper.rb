be3dd8d853865da41ef83fc59e2cec0040a10b94
Michael Robinson <seraph890@gmail.com>
2015-11-11 15:48:34 +1100

    additions to spec and rails helpers for specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'devise'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -49,4 +51,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.include FactoryGirl::Syntax::Methods
+
+  # devise helpers
+  config.include Devise::TestHelpers, type: :controller
+
 end
