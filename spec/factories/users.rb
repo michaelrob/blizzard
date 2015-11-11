adf18dfa4f65a4ac2c8b3e9e40156ba033ee0e7d
Michael Robinson <seraph890@gmail.com>
2015-11-11 17:42:46 +1100

    fixed up factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    username { Faker::Name.last_name }
-    email { Faker::Internet.email(first_name) }
+    username { Faker::Name }
+    email { Faker::Internet.email(username) }
     password { Faker::Internet.password(8) }
     password_confirmation { password }
   end
