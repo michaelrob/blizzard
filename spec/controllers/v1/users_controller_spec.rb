b323662b327f5d1747902efca808154aceed4b13
Michael Robinson <seraph890@gmail.com>
2015-11-11 17:42:39 +1100

    fixed up specs

--- spec/controllers/v1/users_controller_spec.rb
+++ spec/controllers/v1/users_controller_spec.rb
@@ -3,18 +3,36 @@ require 'rails_helper'
 RSpec.describe V1::UsersController, type: :controller do
 
   describe '#create' do
+    let(:params) do
+      {
+        user: {
+          email: 'user_one@exaple.com',
+          username: 'user',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+
     context 'user successful' do
-      let(:user) { create(:user) }
+      before do
+        post :create, params
+      end
 
       it 'should return successful user json' do
-        # do things
-        binding.pry
+        expect(response).to have_http_status :success
       end
     end
 
     context 'user unsuccessful' do
+      before do
+        params[:user][:password_confirmation] = 'wrong_password'
+
+        post :create, params
+      end
+
       it 'should return user_create_error json' do
-        # do things
+        expect(response).to_not have_http_status :success
       end
     end
   end
