d7975e7b66e9e0ef4a0f725a3579eb4082a92333
Michael Robinson <seraph890@gmail.com>
2015-11-19 15:57:45 +1100

    added more sessions specs

--- spec/controllers/v1/sessions_controller_spec.rb
+++ spec/controllers/v1/sessions_controller_spec.rb
@@ -1,25 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe V1::SessionsController, type: :controller do
-  let(:user) { create(:user) }
-
   describe '#create' do
+    let(:user) { create(:user) }
+
     context 'password correct and user present' do
-      it 'should sign_in user' do
-        pending
+      let(:params) do
+        {
+          user: {
+            email: user.email,
+            username: user.username,
+            password: user.password,
+            password_confirmation: user.password_confirmation
+          }
+        }
+      end
+
+      before :each do
+        post :create, params
       end
 
       it 'should render user sign in' do
-        pending
+        expect(response.body).to include user.email
+        expect(response.body).to include user.username
+        expect(response.body).to include user.access_token
       end
     end
 
-    it 'should return error if no user exists' do
-      pending
+    context 'incorrect user details' do
+      let(:params) do
+        {
+          user: {
+            email: user.email,
+            username: user.username,
+            password: user.email
+          }
+        }
+      end
+
+      before :each do
+        post :create, params
+      end
+
+      it 'should return error' do
+        expect(response.body).to include 'sessions_controller.invalid_login_attempt'
+      end
     end
 
-    it 'should return error if password incorrect' do
-      pending
+    context 'user does not exist' do
+      let(:params) do
+        {
+          user: {
+            email: user.password,
+            username: user.password,
+          }
+        }
+      end
+
+      before :each do
+        post :create, params
+      end
+
+      it 'should return error' do
+        expect(response.body).to include 'sessions_controller.invalid_login_attempt'
+      end
     end
   end
 end
