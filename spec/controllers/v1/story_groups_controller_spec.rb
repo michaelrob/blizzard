aa3f56dac8bf2d52a7fa7c9612afd7c75ac2c2de
Michael Robinson <seraph890@gmail.com>
2015-11-25 13:44:43 +1100

    added specs

--- spec/controllers/v1/story_groups_controller_spec.rb
+++ spec/controllers/v1/story_groups_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe V1::StoriesController, type: :controller do
+RSpec.describe V1::StoryGroupsController, type: :controller do
   let(:user) { create(:user) }
 
   describe '#create' do
@@ -8,9 +8,29 @@ RSpec.describe V1::StoriesController, type: :controller do
       {
         story: {
           title: 'new story title',
-          body: 'Story description',
+          description: 'Story description',
         }
       }
+    end
+
+    context 'authorized user' do
+      let!(:authorization) { request.headers['Authorization'] = user.access_token }
+
+      it 'should create new story group' do
+        expect { post :create, params }.to change { StoryGroup.count }.by(1)
+        expect(response.body).to include 'new story title'
+        expect(response.body).to include 'Story description'
+      end
+    end
+
+    context 'unauthorized user' do
+      before do
+        post :create, params
+      end
+
+      it 'should return unauthorized error' do
+        expect(response.body).to include 'translation missing: en.application_controller.unauthorized'
+      end
     end
   end
 end
