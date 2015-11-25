179dfcf36c52579518bc5581def116e59d42755e
Michael Robinson <seraph890@gmail.com>
2015-11-25 11:23:52 +1100

    added story group to story serializer

--- app/serializers/v1/story_serializer.rb
+++ app/serializers/v1/story_serializer.rb
@@ -1,7 +1,7 @@
 module V1
   class StorySerializer < ActiveModel::Serializer
 
-    attributes :id, :title, :body, :user_id
+    attributes :id, :title, :body, :user_id, :story_group_id
 
   end
 end
