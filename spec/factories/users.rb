3073ac5cd4a0aedc5bc0ec430cd0d88022c9df13
Michael Robinson <seraph890@gmail.com>
2017-01-04 11:41:26 +0900

    removed reference to username, will need to work on this because the email address maker is requiring username to generate the address

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    username { Faker::Address.city }
+    # toDo: fix this up, not sure because I don't have internet right now
     email { Faker::Internet.email(username) }
     password { Faker::Internet.password(8) }
     password_confirmation { password }
