9a31ea6c237fd132a070407014b185d16ca25705
Michael Robinson <seraph890@gmail.com>
2017-01-04 11:36:57 +0900

    updated v1 users_controller to be a bit cleanr, moved success and unsuccessful renders to private methods

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -5,16 +5,20 @@ module V1
     def create
       @user = User.new user_params
 
-      if @user.save
-        render json: @user, serializer: V1::SessionSerializer, root: nil
-      else
-        render json: { error: t('user_create_error') }, status: :unprocessable_entity
-      end
+      @user.save ? user_save : user_error
     end
 
 private
     def user_params
       params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    end
+
+    def user_save
+      render json: @user, serializer: V1::SessionSerializer, root: nil
+    end
+
+    def user_error
+      render json: { error: t('user_create_error') }, status: :unprocessable_entity
     end
   end
 end
