55abe93d849ecde3b2826866f9437a884896e67e
Michael Robinson <seraph890@gmail.com>
2017-02-18 22:37:28 +0900

    added some additional tags to the story serialiser

--- app/serializers/v1/story_serializer.rb
+++ app/serializers/v1/story_serializer.rb
@@ -1,7 +1,6 @@
 module V1
   class StorySerializer < ActiveModel::Serializer
 
-    attributes :id, :title, :body, :user_id, :story_group_id
-
+    attributes :id, :title, :body, :user_id, :assigned, :priority, :tags
   end
 end
