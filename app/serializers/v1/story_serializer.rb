50e191469bfb8ff33c144e197053935d654d91a8
Michael Robinson <seraph890@gmail.com>
2017-02-19 21:04:53 +0900

    removed tags and other currently un-needed attributes

--- app/serializers/v1/story_serializer.rb
+++ app/serializers/v1/story_serializer.rb
@@ -1,6 +1,6 @@
 module V1
   class StorySerializer < ActiveModel::Serializer
 
-    attributes :id, :title, :body, :user_id, :assigned, :priority, :tags
+    attributes :id, :title, :body, :user_id, :assigned
   end
 end
