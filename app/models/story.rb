7b036d1f06e8260f248b866dee4b6c69515b8020
Michael Robinson <seraph890@gmail.com>
2017-02-19 20:22:35 +0900

    added belongs too association for story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,7 @@
 class Story < ActiveRecord::Base
   belongs_to :user
   belongs_to :story_group
+  belongs_to :assigned, class_name: "User"
 
   validates :title, presence: true
   validates :body, presence: true
