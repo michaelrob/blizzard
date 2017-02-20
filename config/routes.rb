ddde9ac4a4f1077e183626163d35a27084a4a3df
Michael Robinson <seraph890@gmail.com>
2017-02-20 21:02:14 +0900

    removed index from stories

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
     resources :users, only: [:create]
-    resources :stories, only: [:index, :show, :create]
+    resources :stories, only: [:show, :create]
   end
 end
