019d6678c8dc5d36ed58cb666d0934d16706437d
Michael Robinson <seraph890@gmail.com>
2017-02-20 21:02:28 +0900

    removed index controller method

--- app/controllers/v1/stories_controller.rb
+++ app/controllers/v1/stories_controller.rb
@@ -1,11 +1,6 @@
 module V1
   class StoriesController < ApplicationController
 
-    def index
-      @stories = Story.includes(:user).order(created_at: :desc).all
-      render json: @stories, each_serializer: StoriesSerializer
-    end
-
     def show
       @story = Story.find(params[:id])
       render json: @story, serializer: StorySerializer
